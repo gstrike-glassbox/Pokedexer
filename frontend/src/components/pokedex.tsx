02b05f97c046cf4c4efec534b629a2e37ef83c1a
George Strike <george.strike@outlook.com>
2022-04-06 10:33:20 +0100

    basic search form
    Filter pokemon by name

--- frontend/src/components/pokedex.tsx
+++ frontend/src/components/pokedex.tsx
@@ -4,18 +4,20 @@ import { FC, useEffect, useState } from "react";
 import './pokedex.css';
 
 interface Pokemon {
-    id: number,
-    name: string,
-    maleFrontSprite: string,
-    maleFrontSpriteShiny: string,
-    femaleFrontSprite?: string,
-    femaleFrontSpriteShiny?: string,
+    id: number;
+    [name: string]: string | number | undefined;
+    maleFrontSprite: string;
+    maleFrontSpriteShiny: string;
+    femaleFrontSprite?: string;
+    femaleFrontSpriteShiny?: string;
 }
 
 const Pokedex: FC = () => {
 
     const [pokemon, setPokemon] = useState<Pokemon[]>([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [searchParam] = useState(["name"]);
+    const [q, setQ] = useState("");
 
     const getAllPokemon = async () => {
         setIsLoading(true);
@@ -32,11 +34,38 @@ const Pokedex: FC = () => {
         getAllPokemon();
     }, []);
 
+    const search = (pokemonArr: Pokemon[]) => {
+        return pokemonArr.filter((pokemon: Pokemon) => {
+            return searchParam.some((filteredPokemon) => {
+                return (
+                    // @ts-ignore: Object is possibly 'null'.
+                    pokemon[filteredPokemon]
+                        .toString()
+                        .toLowerCase()
+                        .indexOf(q.toLowerCase()) > -1
+                );
+            });
+        });
+    }
+
     return (
         <div className="site-card-wrapper" style={{ textAlign: 'center', zIndex: 1, width: '100%' }}>
+            <div className="search-wrapper">
+                <label htmlFor="search-form">
+                    <input
+                        type="search"
+                        name="search-form"
+                        id="search-form"
+                        className="search-input"
+                        placeholder="Search for..."
+                        value={q}
+                        onChange={(e) => setQ(e.target.value)}
+                    />
+                    <span className="sr-only">Search Pokemon</span>
+                </label>
+            </div>
             <Row gutter={16}>
-                {console.log(JSON.stringify(pokemon))}
-                {pokemon.length > 0 ? pokemon.map(poke => {
+                {pokemon.length > 0 ? search(pokemon).map(poke => {
                      return <Col span={8}>
                      <Card title={poke.name} bordered={true}>
                          <img src={poke.maleFrontSprite} alt="pokemon-male" />
